82acf0e2002f0cd36b3d4c3e06641de4c94575a4
Dreyark <Dreyark@gmail.com>
2021-04-01 01:44:47 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlbionKillboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
